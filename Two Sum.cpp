dd3fe91a1dfd8546eb302f16ac960a4e0f324feb
Rahul Agarwal <rahul123agarwal99@gmail.com>
2021-10-16 12:15:18 +0530

    Create Two Sum.cpp

---
+++ Two Sum.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+         int n = nums.size();
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        for(int i=0;i<n;i++)
+        {
+            if(mp.find(target-nums[i])!=mp.end())
+            {
+                ans.push_back(mp[target-nums[i]]);
+                ans.push_back(i);
+            }
+            else
+                mp[nums[i]]=i;
+        }
+        return ans;
+    }
+};
